dd5aa605df39c00d41257e304a79c44a1f85a1d8
RaedaS <55299446+RaedaS@users.noreply.github.com>
2019-10-03 14:17:13 -0700

    Update main.java

--- rockpaperscissor/src/main.java
+++ rockpaperscissor/src/main.java
@@ -7,11 +7,17 @@ public class main {
            paper = 2;
            scissors = 3; */
 
+
+      //program generates a random choice from 1 to 3
         int randomNumber = (int)(Math.random()*4 +1);
 
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number (1 for rock, 2 for paper, and 3 for scissors.) ");
+
+        //user input
         int number = input.nextInt();
+
+        //if user chooses rock
 
         if (number == 1 && randomNumber == 1){
             System.out.println("It's a draw, both are rock!");
@@ -26,6 +32,8 @@ public class main {
             System.out.println("I chose scissors. Rock beats scissors! You win!");
         }
 
+        //if user chooses paper
+
         else if(number == 2 && randomNumber == 1){
             System.out.println("I chose rock. Paper beats rock! You win!");
 
@@ -38,6 +46,8 @@ public class main {
             System.out.println("I chose scissors. Scissors beats paper! You lose!");
         }
 
+        //if user chooses scissors
+
         else if (number == 3 && randomNumber == 1){
             System.out.println("I chose rock. Rock beats scissors! You lose!");
         }
@@ -46,8 +56,13 @@ public class main {
             System.out.println("I chose paper. Scissors beat paper. You win!");
         }
 
-        else{
+        else if (number == 3 && randomNumber == 3){
             System.out.println("It's a draw, both are scissors!");
+        }
+
+        //if user inputs an invalid number
+        else {
+            System.out.println("Error, please choose a number from 1-3.");
         }
     }
 
