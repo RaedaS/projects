991f2659dcd0c74e8f63f916bdfe7dc8ada82a34
RaedaS <55299446+RaedaS@users.noreply.github.com>
2019-10-16 14:12:25 -0700

    Add files via upload

---
+++ variables/src/main.java
@@ -0,0 +1,46 @@
+public class main {
+    public static void main(String[] args) {
+
+        //problem 2
+        int room = 113;
+        double deciNum = 2.71828;
+        String compSci = "Computer Science";
+
+        System.out.println("This is room " + room);
+        System.out.println("e is close to " + deciNum);
+        System.out.println("I am learning a bit about " + compSci);
+
+        //mySchedule
+
+        String sub1 = "PreCalculus 11";
+        String teacher1 = "Ms. Ungurean";
+        String sub2 = "Law 12";
+        String teacher2 = "D.Lee";
+        String sub3 = "English 11";
+        String teacher3 = "Mr. Tryssenaar";
+        String sub4 = "Robotics 11";
+        String teacher4 = "Mr. Lai";
+        String sub5 = "Programming 11";
+        String teacher5 = "Mr. Zaremba";
+        String sub6 = "Chemistry 11";
+        String teacher6 = "Mr. Zitka";
+        String sub7 = "Physics 11";
+        String teacher7 = "Mr. Jung";
+        String sub8 = "CLE 10";
+        String teacher8 = "Ms. Jung";
+
+
+        System.out.println("+------------------------------------------------------------+");
+        System.out.println("|1|                    " + sub1 + " : " + teacher1 + "         |");
+        System.out.println("|2|                    " + sub2 + " : " + teacher2 + "                        |");
+        System.out.println("|3|                    " + sub3 + " : " + teacher3 + "           |");
+        System.out.println("|4|                    " + sub4 + " : " + teacher4 + "                 |");
+        System.out.println("|5|                    " + sub5 + " : " + teacher5 + "          |");
+        System.out.println("|6|                    " + sub6 + " : " + teacher6 + "              |");
+        System.out.println("|7|                    " + sub7 + " : " + teacher7 + "                 |");
+        System.out.println("|8|                    " + sub8 + " : " + teacher8 + "                     |");
+        System.out.println("+------------------------------------------------------------+");
+
+
+    }
+}
